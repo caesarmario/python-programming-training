a1223a4c6815594c5513fe559f7ec8bcfa6923a4
Mario Caesar <49087365+caesarmario@users.noreply.github.com>
2022-05-26 09:58:59 +0700

    Add files via upload

---
+++ Meeting 4.1.py
@@ -0,0 +1,83 @@
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
+
+#WAP to print "Hello"
+print ("Hello")
+print ("Welcome to APU")
+print ("This is Python session")
+
+print("Hello.\nWelcome to APU.\n\tThis is Pythoin session")
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
+
+#WAP to accept value in program and display it.
+no=111
+print(no)
+print("no value is: ",no)
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
+
+#WAP to accept value from user and display it.
+no1=int(input("Enter your value here: "))
+print("Your value is: ",no1)
+
+no2=float(input("Enter decimal value here: "))
+print("Your decimal value is: ",no2)
+
+name=str(input("Enter your name: "))
+print("Your name is: ",name)
+
+last=input("Enter your last name: ")
+print("Your last name is: ",last)
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
+
+#WAP to accept 2 numbers from user and display addition of those numbers.
+no1=int(input("Enter your first number: "))
+no2=int(input("Enter your second number: "))
+add = no1+no2
+print("Your first number is: ",no1,"\nYour second number is:",no2)
+print("Your addition is: ",add)
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
+
+#WAP to accept 2 numbers from user and display output for all
+#arithmetic operators (+,-,*,/,%,//,**)
+no1=int(input("Enter your 1st number: "))
+no2=int(input("Enter your 2nd number: "))
+add = no1+no2
+sub = no1-no2
+mul = no1*no2
+div = no1/no2
+mod = no1%no2
+divint = no1//no2
+pwr = no1**no2
+print("\nAll arithmetic outputs are")
+print("Your addition is\t\t: ",add)
+print("Your Substraction is\t\t: ",sub)
+print("Your Multiplication is\t\t: ",mul)
+print("Your Division is\t\t: ",round(div,2)) #dibuat jadi 2 angka belakang koma
+print("Your Modulus is\t\t\t: ",mod)
+print("Your Division with Integer is\t: ",divint)
+print("Your Power is\t\t\t: ",pwr)
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
+
+#WAP to accept 2 numbers from user and display swapped values of that numbers.
+no1=int(input("Enter your 1st number: "))
+no2=int(input("Enter your 2nd number: "))
+temp=no1
+no1=no2
+no2=temp
+#no1,no2=no2,no1
+print("\nBefore swapping values are: \tno1 is:",no1,"\tno2 is",no2)
+print("After swapping values are : \tno1 is:",no2,"\tno2 is",no1)
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
---
+++ Meeting 4.2.py
@@ -0,0 +1,51 @@
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
+
+#Write a program to check given number is positive
+no1=int(input("Enter your value here: "))
+if(no1>0):
+    print("Your number is positive.")
+print("Done")
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
+
+#Write a program to check given number is positive or negative
+no1=int(input("Enter your value here: "))
+if(no1>0):
+    print("Your number is positive.")
+else:
+    print("Your number is negative")
+print("Done")
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
+
+#Write a program to check given number is 0, positive or negative
+no1=int(input("Enter your value here: "))
+if(no1==0):
+    print("Your number is zero.")
+elif(no1>0):
+    print("Your number is positive.")
+else:
+    print("Your number is negative")
+print("Done")
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
+
+#Write a program to display largest number between 2 numbers
+no1=int(input("Enter your 1st number: "))
+no2=int(input("Enter your 2nd number: "))
+if(no1==0 and no2==0):
+    print("All Zero")
+elif (no1==no2):
+    print("All Equal")
+elif (no1>no2):
+    print("no1 is largest")
+else:
+    print("no2 is largest")
+# Mario Caesar
+# linktr.ee/caesarmario_
+# 5 Feb 2021
